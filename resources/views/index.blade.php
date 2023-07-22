46865fc14f4d7aaf58c250f724f2f355e3ac9eb0
mszaman <munshisayeduzzaman@gmail.com>
2023-07-23 03:40:27 +0600

    Create HomeController, modified public/css/style.css, rename welcome.blade.php file to index.blade.php, create resources/views/layouts/navbar.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="main-section">
+        <div class="main-wrapper">
+            <h1>LangingPage</h1>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,33 @@
+<header class="header">
+    <nav class="nav">
+      <a href="{{ route('home') }}" class="nav-logo">CodeBlogs</a>
+      <ul class="nav-lists">
+        <li class="nav-list">
+          <a href="{{ route('home') }}" class="nav-link">home</a>
+        </li>
+        <li class="nav-list">
+          <a id="dashAnc" href="./pages/dashboard.html" class="nav-link"
+            >dashboard</a
+          >
+        </li>
+        <li class="nav-list">
+          <a href="./pages/profile.html" class="nav-link">profile</a>
+        </li>
+        <li class="nav-list">
+          <a href="./pages/posts.html" class="nav-link">posts</a>
+        </li>
+        <li class="nav-list">
+          <a href="./pages/tags.html" class="nav-link">tags</a>
+        </li>
+        <li class="nav-list">
+          <a href="./pages/signin.html" class="nav-link">signin</a>
+        </li>
+        <li class="nav-list">
+          <a href="./pages/signup.html" class="nav-link">signup</a>
+        </li>
+        <li class="nav-list">
+          <a href="./index.html" class="nav-link">signout</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
